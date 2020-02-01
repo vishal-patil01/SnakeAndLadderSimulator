bca55c8c1f6ca9b9044e5fa6f0b2172941da14e3
Vishal Patil <vishalpatil01@hotmail.com>
2020-02-01 16:23:53 +0530

    Added :stored number of times die rolled and player position

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -1,48 +1,62 @@
 #!/bin/bash 
 echo "Welcome to Snake And Ladder Simulator"
 
-#!Initialize variables & constants
-playerPosition=0
+#!Initialize Constants
 NO_PLAY=0
 LADDER=1
 SNAKE=2
 
+#!Initializing Variables
+playerPosition=0
+diceRoll=0
+declare -A gameStatistics
+
 #!Function To set playerPosition according to playing Options like NO_Play or Snake or Ladder
 function setPlayerMoves() {
 
-while [ $playerPosition -le 100  ]
-do
 	#!Storing dice values between 1 to 6 & set playing Options
-
 	dieValue=$((RANDOM % 6 + 1))
 	playingOptions=$((RANDOM % 3))
+	#!increment Dice Count
+	((diceRoll++))
 
 	#! Increment player Position according to $playingOptions
-
 	case $playingOptions in
-
 		$NO_PLAY)
-			playerPosition=$playerPosition
-			;;
+				playerPosition=$playerPosition
+				;;
 		$LADDER)
-			playerPosition=$((playerPosition + dieValue))
-			;;
+				playerPosition=$((playerPosition + dieValue))
+				;;
 		$SNAKE)
-			playerPosition=$((playerPosition - dieValue))
-			;;
+				playerPosition=$((playerPosition - dieValue))
+				;;
 	esac
 
-		#! Ensures playerPosition is not below 0
+		#!Reset player Position if position is not between 0 to 100 
+		resetWrongPosition
+		#!storing diceCount and player positions in Dictionary
+		gameStatistics[DiceRoll:$diceRoll]=$playerPosition
+}
 
-		if [ $playerPosition -lt 0 ]
-		then
-			playerPosition=0
-		fi
+function playUntilWin() {
+	while [ $playerPosition -ne 100  ]
+	do
+		setPlayerMoves
+	done
+}
 
-	echo "Player Position : "$playerPosition
-done
-
+function resetWrongPosition {
+	#! Ensures playerPosition is not below 0
+	if [ $playerPosition -lt 0 ]
+	then
+		playerPosition=0
+	elif [ $playerPosition -gt 100 ]
+	then
+		playerPosition=$((playerPosition - dieValue))
+	fi
 }
 
 #!Start game
-setPlayerMoves
+playUntilWin
+
